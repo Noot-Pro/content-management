e336a73cf8abe0ee0cad94ab661230b3b0c57a58
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2025-11-09 23:00:53 +0300

    wip: prepare website

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=KoHo:ital,wght@0,200;0,300;0,500;0,700;1,200;1,300;1,600;1,700&display=swap" rel="stylesheet">
 
     @livewireStyles
-{{--    @filamentStyles--}}
+    @filamentStyles
     @stack('styles')
 
     <script src="https://cdn.tailwindcss.com"></script>
@@ -73,7 +73,7 @@
     </header>
 
 <div class="container mx-auto my-6">
-{{--    {{ $slot }}--}}
+    {{ $slot }}
 </div>
 
 <footer class="bg-gray-100 dark:bg-gray-800 p-6 text-center font-light">
@@ -81,8 +81,8 @@
 </footer>
 
 @livewireScripts
-{{--@filamentScripts--}}
-{{--@livewire('notifications')--}}
+@filamentScripts
+@livewire('notifications')
 @stack('scripts')
 
 <script>
