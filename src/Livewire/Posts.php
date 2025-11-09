7386faa22991e73556402c8a5c9f58f8f9b434bb
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2025-11-09 15:10:52 +0300

    wip: prepare website

--- src/Livewire/Posts.php
+++ src/Livewire/Posts.php
@@ -3,8 +3,10 @@
 namespace NootPro\ContentManagement\Livewire;
 
 use Illuminate\View\View;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('components.layouts.app')]
 class Posts extends Component
 {
     use SearchHelpers;
@@ -62,7 +64,6 @@ class Posts extends Component
                     ->where('type', 'category')
                     ->get(),
                 'stickies' => config('noot-pro-content-management.models.Post')::with(['author', 'media'])->sticky()->published()->get(),
-            ])
-            ->layout(config('noot-pro-content-management.layout'));
+            ]);
     }
 }
