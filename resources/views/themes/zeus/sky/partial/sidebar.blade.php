9953e6002a4c96ee73b50e50800541c30dd8da06
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2025-11-10 10:45:00 +0300

    wip: prepare website

--- resources/views/themes/zeus/sky/partial/sidebar.blade.php
+++
@@ -1,5 +0,0 @@
-{{--@include($themePath.'.partial.authors')--}}
-@include($themePath.'.partial.sidebar.search')
-@include($themePath.'.partial.sidebar.categories')
-@include($themePath.'.partial.sidebar.recent')
-@include($themePath.'.partial.sidebar.pages')
