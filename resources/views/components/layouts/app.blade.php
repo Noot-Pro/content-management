4ff6ba7bd1054864d38d1559ebd5cfffc3e0e7f3
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2025-11-10 13:49:21 +0300

    wip: prepare website

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -63,7 +63,14 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex sm:items-center">
-                    {{--Navigation Links--}}
+                    <a href="{{ route('posts') }}" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition-colors">
+                        {{ __('News') }}
+                    </a>
+                    @if(Route::has('faq'))
+                        <a href="{{ route('faq') }}" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition-colors">
+                            {{ __('FAQ') }}
+                        </a>
+                    @endif
                 </div>
 
             </div>
