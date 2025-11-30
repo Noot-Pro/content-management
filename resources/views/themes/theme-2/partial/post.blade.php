f358c692e708ec0c4db5e0b38b237c4b1d397c1e
aymanalhalali <aymanalhalali@gmail.com>
2025-11-30 11:21:31 +0300

    fix direction of category

--- resources/views/themes/theme-2/partial/post.blade.php
+++ resources/views/themes/theme-2/partial/post.blade.php
@@ -1,6 +1,7 @@
 <article class="mb-6 bg-white shadow-md p-6 rounded-lg ltr:rounded-tr-[2rem] ltr:rounded-bl-[2rem] rtl:rounded-tl-[2rem] rtl:rounded-br-[2rem]">
     @unless ($post->tags->where('type','category')->isEmpty())
         <div class="flex items-start justify-between mb-4">
+            <span class="font-light text-sm text-gray-600">{{ optional($post->published_at)->diffForHumans() ?? '' }}</span>
             <div>
                 @foreach($post->tags->where('type','category') as $category)
                     <a href="{{ route('tags',[$category->type,$category->slug]) }}" class="inline-block px-4 py-2 bg-orange-500 text-white text-sm font-medium rounded-lg hover:bg-orange-600 transition-colors">
@@ -8,7 +9,6 @@
                     </a>
                 @endforeach
             </div>
-            <span class="font-light text-sm text-gray-600">{{ optional($post->published_at)->diffForHumans() ?? '' }}</span>
         </div>
     @else
         <div class="mb-3">
